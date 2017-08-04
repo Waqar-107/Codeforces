fe2441ece49548f1a922db6c83a47a6a21b00142
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2017-08-04 15:51:51 +0700

    Create 837C.Two Seals.cpp

---
+++ C-set/837C.Two Seals.cpp
@@ -0,0 +1,72 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+
+#define dbg printf("in\n");
+#define nl printf("\n");
+#define pp pair<int,int>
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+
+    int i,j,k;
+    int n,m,r;
+    int a,b,c;
+    int x,y;
+
+    cin>>n>>a>>b;
+
+    vector<pp> v;
+    for(i=0;i<n;i++)
+    {
+        cin>>x>>y;
+
+        if(x<=a && y<=b)
+        {
+            v.push_back({x,y});
+            continue;
+        }
+
+        else if(y<=a && x<=b)
+        {
+            v.push_back({x,y});
+        }
+    }
+
+    m=0;
+    for(i=0;i<v.size()-1;i++)
+    {
+        for(j=i+1;j<v.size();j++)
+        {
+            k=(v[i].first*v[i].second)+(v[j].first*v[j].second);
+
+            //keep v[i] as it is
+            r=a;c=b-v[i].second;
+            if(v[i].first<=a && v[i].second<=b && r>=0 && c>=0 && ((v[j].first<=r && v[j].second<=c) || (v[j].first<=c && v[j].second<=r)))
+                m=max(m,k);
+
+            r=a-v[i].first;c=b;
+            if(v[i].first<=a && v[i].second<=b && r>=0 && c>=0 && ((v[j].first<=r && v[j].second<=c) || (v[j].first<=c && v[j].second<=r)))
+                m=max(m,k);
+
+            //rotate
+            r=a;c=b-v[i].first;
+            if(v[i].first<=b && v[i].second<=a && r>=0 && c>=0 && ((v[j].first<=r && v[j].second<=c) || (v[j].first<=c && v[j].second<=r)))
+                m=max(m,k);
+
+            r=a-v[i].second;c=b;
+            if(v[i].first<=b && v[i].second<=a && r>=0 && c>=0 && ((v[j].first<=r && v[j].second<=c) || (v[j].first<=c && v[j].second<=r)))
+                m=max(m,k);
+
+        }
+    }
+
+    cout<<m;
+
+    return 0;
+}
