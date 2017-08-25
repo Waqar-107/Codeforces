3f7a121706c7d02a90835c96099e36f0954717e0
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2017-08-25 16:15:43 +0700

    Update 653A.Bear and Three Balls.py

--- A-set/653A.Bear and Three Balls.py
+++ A-set/653A.Bear and Three Balls.py
@@ -3,9 +3,15 @@
 n=int(input())
 a=list(map(int,input().split()))
 
+x=set(a)
+a.clear()
+
+for i in x:
+    a.append(i)
+
 a.sort()
 
-for i in range(2,n):
+for i in range(2,len(a)):
     if abs(a[i]-a[i-1])<=2 and abs(a[i]-a[i-2])<=2 and abs(a[i-1]-a[i-2])<=2 and a[i]!=a[i-1] and a[i]!=a[i-2] and a[i-1]!=a[i-2]:
         print("YES")
         exit(0)
