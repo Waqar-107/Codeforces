09355edcdbf1d58efc5b5638894bc45f93d4e2c5
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-03-22 11:43:28 +0700

    Create 66D. Petya and His Friends.py

---
+++ D-set/66D. Petya and His Friends.py
@@ -0,0 +1,20 @@
+# from dust i have come dust i will be
+
+import math
+n=int(input())
+
+if n==2:
+    print(-1)
+    exit(0)
+
+print(15)
+print(10)
+print(12)
+
+n-=3
+m=2
+
+while n>0:
+    n-=1
+    print(12*m)
+    m+=1
