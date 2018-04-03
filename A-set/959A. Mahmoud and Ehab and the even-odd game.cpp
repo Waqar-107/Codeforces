8a66cc35c1795dbbd6792903e9f37dbf24c2cac2
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-04-04 00:23:16 +0600

    Create 959A. Mahmoud and Ehab and the even-odd game.cpp

---
+++ A-set/959A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 150
+#define inf 1000000
+#define pp pair<int,int>
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d\n",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+#define pb(n) push_back(n)
+
+using namespace std;
+
+int main()
+{
+
+    //freopen("in.txt", "r", stdin);
+
+    int i,j,k;
+    int n,m;
+
+    sf(n);
+
+    if(n%2==0)
+        pfs("Mahmoud");
+    else
+        pfs("Ehab");
+
+    return 0;
+}
