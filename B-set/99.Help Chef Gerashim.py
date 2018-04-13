9ab0fad03e4c888710afbef9a90f0c3747d23e36
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-04-13 15:04:23 +0600

    Create 99.Help Chef Gerashim.py

---
+++ B-set/99.Help Chef Gerashim.py
@@ -0,0 +1,32 @@
+# from dust i have come dust i will be
+
+n=int(input())
+a=[0]*n
+
+for i in range(n):
+    a[i]=int(input())
+
+avg=sum(a)//n
+
+x=[]
+for i in range(n):
+    if a[i]!=avg:
+        x.append(i)
+
+if len(x)==0:
+    print('Exemplary pages.')
+
+elif len(x)==2:
+    if (a[x[0]]+a[x[1]])//2==avg:
+        d=abs(a[x[0]]-a[x[1]])//2
+
+        if a[x[0]]>a[x[1]]:
+            print(str(d)+' ml. from cup #'+str(x[1]+1)+' to cup #'+str(x[0]+1)+'.')
+
+        else:
+            print(str(d)+' ml. from cup #'+str(x[0]+1)+' to cup #'+str(x[1]+1)+'.')
+    else:
+        print('Unrecoverable configuration.')
+
+else:
+    print('Unrecoverable configuration.')
