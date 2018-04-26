156888691c10e77a268b83c2d2d7288fb58f2efe
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-04-26 10:13:44 +0600

    Create 965A.Paper Airlines.cpp

---
+++ A-set/965A.Paper Airlines.cpp
@@ -0,0 +1,66 @@
+/***from dust i have come, dust i will be***/
+
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<map>
+#include<numeric>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<utility>
+#include<vector>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 5000
+#define inf 1000000
+#define pp pair<int,int>
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d ",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s\n",s)
+
+#define pb push_back
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i,j,k;
+    int n,m;
+    int s,p;
+
+    sff(k,n);sff(s,p);
+
+    if(n%s==0)
+        n=n/s;
+    else
+        n=n/s+1;
+
+    n=k*n;
+    if(n%p==0)
+        m=n/p;
+    else
+        m=n/p+1;
+
+    pf(m);
+
+    return 0;
+}
