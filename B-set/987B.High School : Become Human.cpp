75fb3c9312a4f741c269f3bc8bd28e5c05c587d6
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-05-30 14:27:39 +0600

    Create 987B.High School : Become Human.cpp

---
+++ B-set/987B.High School : Become Human.cpp
@@ -0,0 +1,71 @@
+/***from dust i have come, dust i will be***/
+
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<map>
+#include<numeric>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<utility>
+#include<vector>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 50700
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d ",n)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i,j,k;
+    int n,m;
+    ll x,y;
+
+    cin>>x>>y;
+
+    if(x==y)
+    {
+        cout<<"=";
+        return 0;
+    }
+    
+    double a,b;
+
+    a=y*log(x);
+    b=x*log(y);
+
+    if(a==b)
+        cout<<"=";
+
+    else if(a>b)
+        cout<<">";
+
+    else
+        cout<<"<";
+
+    return 0;
+}
