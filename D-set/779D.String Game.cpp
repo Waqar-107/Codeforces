7a04b87ff5f45914e51638e9e6131f935b701b63
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-08-14 02:59:07 +0600

    Create 779D.String Game.cpp

---
+++ D-set/779D.String Game.cpp
@@ -0,0 +1,78 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 5100
+#define inf 100000000
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d ",n)
+#define pff(n,m) printf("%d %d\n",n,m)
+#define pffl(n,m) printf("%I64d %I64d\n",n,m)
+#define pfl(n) printf("%I64d ",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int,int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i,j,k;
+    int n,m;
+
+    string s,t;
+    cin>>s>>t;
+
+    n=s.length();
+    int a[n];
+
+    for(i=0; i<n; i++)
+        sf(a[i]);
+
+    m=t.length();
+    int high=n, low=0, mid,ans=0;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+
+        string temp=s;
+        for(i=0; i<mid; i++)
+            temp[a[i]-1]='#';
+
+        i=0;
+        j=0;
+        while(i<m && j<n)
+        {
+            if(temp[j]==t[i])
+                i++;
+
+            j++;
+        }
+
+        if(i==m)
+        {
+            ans=max(ans,mid);
+            low=mid+1;
+        }
+
+        else
+            high=mid-1;
+    }
+
+    pf(ans);
+
+    return 0;
+}
