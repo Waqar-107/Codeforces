918f4f87b6e0dceb0c2e93f255fccb4163ce43e4
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-09-03 00:03:05 +0600

    Create 1037A.Packet.cpp

---
+++ A-set/1037A.Packet.cpp
@@ -0,0 +1,46 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 300110
+#define inf 1000000000
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d ",n)
+#define pff(n,m) printf("%d %d\n",n,m)
+#define pffl(n,m) printf("%I64d %I64d\n",n,m)
+#define pfl(n) printf("%I64d\n",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int,int>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    int n,m;
+
+    sf(n);
+
+    int cnt=0;
+    while(n)
+    {
+        cnt++;
+        n=n>>1;
+    }
+
+    pf(cnt);
+
+    return 0;
+}
