8ff835c7d8559847e808a48aca49ee8ef626b579
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-10-14 22:00:05 +0600

    Create 1064C.Oh those Palindromes.cpp

---
+++ C-set/1064C.Oh those Palindromes.cpp
@@ -0,0 +1,44 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 101010
+#define inf 100000000
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d\n",n,m)
+#define pffl(n,m) printf("%I64d %I64d\n",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int,int>
+#define mod 1000000007
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i,j,k;
+    int n,m,t;
+    string s;
+
+    cin>>n>>s;
+
+    sort(s.begin(),s.end());
+    cout<<s;
+
+    return 0;
+}
