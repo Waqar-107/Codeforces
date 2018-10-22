166a7c39531d1cb2568ee1fc56f921b41b3d46eb
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-10-23 00:42:04 +0600

    Create 184.Patties.cpp

---
+++ ACMSGURU/184.Patties.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/***from dust i have come, dust i will be***/
+
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<map>
+#include<numeric>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<utility>
+#include<vector>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n");
+#define nl printf("\n");
+#define N 100100
+#define inf 1e18
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int,int>
+
+using namespace std;
+
+int main()
+{
+	//freopen("in2.txt", "r", stdin);
+
+	int i, j, k;
+	int n, m;
+
+	int p, c;
+	int r, v;
+
+	sff(p, m); sf(c);
+	sff(k, r); sf(v);
+
+	int ans = min(p / k, min(m / r, c / v));
+
+	pf(ans);
+
+	return 0;
+}
