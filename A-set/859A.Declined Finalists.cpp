de167fa5939377a7dc93b46248f6a88c40696d3f
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2018-11-09 00:05:10 +0600

    Create 859A.Declined Finalists.cpp

---
+++ A-set/859A.Declined Finalists.cpp
@@ -0,0 +1,49 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n");
+#define nl printf("\n");
+#define N 100100
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int,int>
+
+using namespace std;
+
+int main()
+{
+	freopen("in.txt", "r", stdin);
+
+	int i,j,k;
+	int n,m;
+
+	sf(n);
+
+	m=0;
+	for(i=0;i<n;i++)
+        sf(k), m=max(m,k);
+
+    if(m<=25)
+        pfs("0");
+
+    else
+        pf(m-25);
+
+
+	return 0;
+}
