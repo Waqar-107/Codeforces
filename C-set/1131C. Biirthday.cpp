7c79237adc5bbfcdcc8f4cf237824a08e4a0303a
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-02-23 18:39:30 +0600

    Create 1131C. Biirthday.cpp

---
+++ C-set/1131C. Biirthday.cpp
@@ -0,0 +1,62 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 2500
+#define inf 1e6
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int,int>
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+
+    sf(n);
+
+    int a[n];
+    for(i = 0; i < n; i++)sf(a[i]);
+
+    sort(a,a+n);
+
+    int ans[n];
+
+    i = 0, j = n - 1;
+    for(k=0;k<n;k+=2)
+    {
+        if(i == j){
+            ans[i]=a[k];
+            break;
+        }
+
+        ans[i]=a[k];
+        ans[j]=a[k+1];
+        i++;j--;
+    }
+
+    for(i=0;i<n;i++)
+        pf(ans[i]), pfs(" ");
+
+    return 0;
+}
