19359db338f7619a5f68fe2f1d1f7cf42599a18e
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-05-15 15:55:51 +0600

    Create 1165B. Polycarp Training.cpp

---
+++ B-set/1165B. Polycarp Training.cpp
@@ -0,0 +1,56 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1000100
+#define inf 1e18
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+
+    sf(n);
+    int a[n];
+
+    for(i = 0;i < n; i++)
+        sf(a[i]);
+
+    sort(a,a+n);
+
+    k = 1, m = 0, i = 0;
+    while(i < n)
+    {
+        if(a[i] >= k)
+            k++, i++, m++;
+        else
+            i++;
+    }
+
+    pf(m);
+
+    return 0;
+}
