6db46c28541d50c69a6ae237af444c788b51173b
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-06-08 16:32:13 +0600

    Create 1173B. Nauuo and Chess.cpp

---
+++ B-set/1173B. Nauuo and Chess.cpp
@@ -0,0 +1,49 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 200100
+#define inf 1e18
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int a[N];
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+
+    sf(n);
+
+    m = n / 2 + 1;
+    pf(m), nl;
+
+    for(i = 1; i <=m; i++)
+        pff(i, 1), nl;
+
+    for(i = 2; i <= n - m + 1; i++)
+        pff(m, i), nl;
+
+    return 0;
+}
