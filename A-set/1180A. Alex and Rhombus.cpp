7197bdc1b4bd7f9117363f77bf33c3a47fb7ca2c
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-06-23 22:12:51 +0600

    Create 1180A. Alex and Rhombus.cpp

---
+++ A-set/1180A. Alex and Rhombus.cpp
@@ -0,0 +1,50 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1000100
+#define inf 1e18
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+
+    sf(n);
+
+    ll ans = 0, p = 1;
+    for(i = 1; i <= n - 1; i++)
+    {
+        ans += p * 2;
+        p += 2;
+    }
+
+    ans += p;
+
+    pfl(ans);
+
+    return 0;
+}
