35fa44314d19af485202ead85fda761e16b6d6cb
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-08-20 18:39:00 +0600

    Create 1201A. Zero Array.cpp

---
+++ B-set/1201A. Zero Array.cpp
@@ -0,0 +1,55 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 201010
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, mx = 0;
+
+    sf(n);
+
+    ll sum = 0;
+    for(i = 0; i < n; i++)
+        sf(m), sum += m, mx = max(m, mx);
+
+    if(sum % 2)
+    {
+        pfs("NO\n");
+        return 0;
+    }
+
+    if(2 * mx <= sum)
+        pfs("YES");
+    else
+        pfs("NO\n");
+
+    return 0;
+}
