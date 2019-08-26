0d0ddbe50cb9717203648034b4ceee4ad7ce2f61
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-08-26 21:55:33 +0600

    Create 1208A. Xorinacci.cpp

---
+++ A-set/1208A. Xorinacci.cpp
@@ -0,0 +1,57 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 35
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, q;
+    int a, b;
+
+    sf(q);
+    while(q--)
+    {
+        sff(a, b), sf(n);
+
+        m = a ^ b;
+        if((n + 1) % 3 == 1)
+            pf(a);
+
+        else if((n + 1) % 3 == 2)
+            pf(b);
+
+        else
+            pf(m);
+
+        nl;
+    }
+
+    return 0;
+}
