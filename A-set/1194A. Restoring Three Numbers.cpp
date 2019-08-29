a5298ae909bf9ef32256de5d2316660097d6cec4
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-08-29 19:42:42 +0600

    Create 1194A. Restoring Three Numbers.cpp

---
+++ A-set/1194A. Restoring Three Numbers.cpp
@@ -0,0 +1,48 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1010
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, q;
+
+    int arr[4];
+    for(i = 0; i < 4; i++)
+        sf(arr[i]);
+
+    sort(arr, arr + 4);
+
+    for(i = 2; i >= 0; i--)
+        pf(arr[3] - arr[i]), pfs(" ");
+
+
+    return 0;
+}
