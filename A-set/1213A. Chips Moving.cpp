04a8001c57ccf5189e1c33c186899a9e4968e1e8
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-08-31 18:23:01 +0600

    Create 1213A. Chips Moving.cpp

---
+++ A-set/1213A. Chips Moving.cpp
@@ -0,0 +1,53 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1010
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+
+    sf(n);
+    int odd = 0, ev = 0;
+
+    while(n--)
+    {
+        sf(m);
+
+        if(m % 2)
+            odd++;
+        else
+            ev++;
+    }
+
+    pf(min(ev, odd));
+
+    return 0;
+}
