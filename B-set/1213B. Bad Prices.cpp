665b4583925d85625392aa3b99f7b64b6b78cfe5
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-09-01 21:56:08 +0600

    Create 1213B. Bad Prices.cpp

---
+++ B-set/1213B. Bad Prices.cpp
@@ -0,0 +1,58 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 150010
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int a[N];
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+        for(i = 0; i < n; i++)sf(a[i]);
+
+        int cnt = 0, mn = a[n - 1];
+        for(i = n - 2; i >= 0; i--)
+        {
+            if(a[i] > mn)
+                cnt++;
+
+            mn = min(a[i], mn);
+        }
+
+        pf(cnt);nl;
+    }
+
+    return 0;
+}
