3b8cb92f81c3aef1e948fb84ba36ecf4e2dc295d
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-09-23 13:40:59 +0600

    Create 1221C. Perfect Team.cpp

---
+++ C-set/1221C. Perfect Team.cpp
@@ -0,0 +1,58 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 35
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, q, c, x, cnt;
+
+    sf(q);
+    while(q--)
+    {
+        sff(c, m); sf(x);
+
+        x += abs(c - m);
+        k = min(c, m);
+
+        c = m = k;
+
+        cnt = min(k, x);
+
+        c -= cnt; m -= cnt;
+        cnt += (c + m) / 3;
+
+        pf(cnt);
+        nl;
+    }
+
+
+    return 0;
+}
