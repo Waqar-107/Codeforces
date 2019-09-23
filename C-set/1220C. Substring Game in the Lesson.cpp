3d0e994d1a52a3e0aeadfa8f26e34f3eb7bb71a3
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-09-23 17:33:40 +0600

    Create 1220C. Substring Game in the Lesson.cpp

---
+++ C-set/1220C. Substring Game in the Lesson.cpp
@@ -0,0 +1,56 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 110
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, q;
+    string s;
+
+    cin >> s;
+
+    n = s.length();
+
+    cout << "Mike\n";
+    m = s[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(m < s[i])
+            cout << "Ann\n";
+        else
+            cout << "Mike\n";
+
+        m = min(m, (int)s[i]);
+    }
+
+    return 0;
+}
