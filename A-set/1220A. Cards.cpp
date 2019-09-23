d467eae27e5998062fad18aa95864abdb00659ee
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-09-23 15:43:27 +0600

    Create 1220A. Cards.cpp

---
+++ A-set/1220A. Cards.cpp
@@ -0,0 +1,54 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 110
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, q;
+    string s;
+
+    sf(n);
+    cin >> s;
+
+    int z = 0; m = 0;
+    for(i = 0; i < n; i++)
+    {
+        if(s[i] == 'z') z++;
+        else if(s[i] == 'n') m++;
+    }
+
+    while(m--)
+        pf(1), pfs(" ");
+    while(z--)
+        pf(0), pfs(" ");
+
+    return 0;
+}
