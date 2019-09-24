157126d9f24f9cf6adaf62a2c3a7be06ff865e9f
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-09-24 12:18:27 +0600

    Create 1148A. Another One Bites The Dust.cpp

---
+++ A-set/1148A. Another One Bites The Dust.cpp
@@ -0,0 +1,49 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 110
+#define inf 1e18
+#define mod 100000007
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, q;
+    ll a, b, c;
+
+    sffl(a, b);
+    sfl(c);
+
+    ll ans = (2 * c) + (min(a, b) * 2);
+    if(a != b)
+        ans++;
+
+    pfl(ans);
+
+
+    return 0;
+}
