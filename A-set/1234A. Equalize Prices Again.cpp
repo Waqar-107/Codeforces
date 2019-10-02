68a4e6a50ed3876d1aecaa83717cbd327c03e2d2
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-10-02 17:45:43 +0600

    Create 1234A. Equalize Prices Again.cpp

---
+++ A-set/1234A. Equalize Prices Again.cpp
@@ -0,0 +1,54 @@
+    /***from dust i have come, dust i will be***/
+     
+    #include<bits/stdc++.h>
+     
+    typedef long long int ll;
+    typedef unsigned long long int ull;
+     
+    #define dbg printf("in\n")
+    #define nl printf("\n")
+    #define N 1010101
+    #define inf 1e18
+    #define mod 100000007
+     
+    #define sf(n) scanf("%d", &n)
+    #define sff(n,m) scanf("%d%d",&n,&m)
+    #define sfl(n) scanf("%I64d", &n)
+    #define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+     
+    #define pf(n) printf("%d",n)
+    #define pff(n,m) printf("%d %d",n,m)
+    #define pffl(n,m) printf("%I64d %I64d",n,m)
+    #define pfl(n) printf("%I64d",n)
+    #define pfs(s) printf("%s",s)
+     
+    #define pb push_back
+    #define pp pair<int, int>
+     
+    using namespace std;
+     
+    int main()
+    {
+        //freopen("in.txt", "r", stdin);
+     
+        int i, j, k;
+        int n, m, q;
+     
+        sf(q);
+        while(q--)
+        {
+            sf(n);
+     
+            ll sum = 0;
+            for(i = 0; i < n; i++)
+                sf(m), sum += m;
+     
+            if(sum % n == 0)
+                pfl(sum / n);
+            else
+                pfl(sum / n + 1);
+            nl;
+        }
+     
+        return 0;
+    }
