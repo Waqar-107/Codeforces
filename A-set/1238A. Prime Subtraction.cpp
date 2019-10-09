56c63ede679f62f373e1d85c2546993f8a08eb1a
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-10-09 16:15:39 +0600

    Create 1238A. Prime Subtraction.cpp

---
+++ A-set/1238A. Prime Subtraction.cpp
@@ -0,0 +1,51 @@
+    /***from dust i have come, dust i will be***/
+     
+    #include<bits/stdc++.h>
+     
+    typedef long long int ll;
+    typedef unsigned long long int ull;
+     
+    #define dbg printf("in\n")
+    #define nl printf("\n")
+    #define N 1010101
+    #define inf 1e18
+    #define mod 998244353
+     
+    #define sf(n) scanf("%d", &n)
+    #define sff(n,m) scanf("%d%d",&n,&m)
+    #define sfl(n) scanf("%I64d", &n)
+    #define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+     
+    #define pf(n) printf("%d",n)
+    #define pff(n,m) printf("%d %d",n,m)
+    #define pffl(n,m) printf("%I64d %I64d",n,m)
+    #define pfl(n) printf("%I64d",n)
+    #define pfs(s) printf("%s",s)
+     
+    #define pb push_back
+    #define pp pair<int, int>
+     
+    using namespace std;
+     
+    int main()
+    {
+        //freopen("in.txt", "r", stdin);
+     
+        int i, j, k;
+        int n, m, q;
+        ll x, y;
+     
+        sf(q);
+        while(q--)
+        {
+            sffl(x, y);
+     
+            if(abs(x - y) == 1)
+                pfs("NO\n");
+            else
+                pfs("YES\n");
+        }
+     
+        return 0;
+    }
+
