ce03a2fee5d2062740bbd10055e2fb783413769c
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-10-15 16:25:09 +0600

    Create 1178C. Tiles.cpp

---
+++ C-set/1178C. Tiles.cpp
@@ -0,0 +1,49 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 101010
+#define inf 1e18
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+
+    sff(n, m);
+
+    ll mod = 998244353;
+    ll e = n + m, p = 1;
+
+    while(e--)
+    {
+        p = ((p % mod) * 2) % mod;
+    }
+
+    cout << p; nl;
+
+    return 0;
+}
