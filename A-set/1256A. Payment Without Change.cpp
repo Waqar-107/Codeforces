944cac8a0eb170d4908c7cbf63c70fc9c1472ffb
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-11-05 12:38:55 +0600

    Create 1256A. Payment Without Change.cpp

---
+++ A-set/1256A. Payment Without Change.cpp
@@ -0,0 +1,54 @@
+    /***from dust i have come, dust i will be***/
+     
+    #include<bits/stdc++.h>
+     
+    typedef long long int ll;
+    typedef unsigned long long int ull;
+     
+    #define dbg printf("in\n")
+    #define nl printf("\n")
+    #define N 10000100
+    #define inf 1e9
+     
+    #define sf(n) scanf("%d", &n)
+    #define sff(n,m) scanf("%d%d",&n,&m)
+    #define sfl(n) scanf("%I64d", &n)
+    #define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+     
+    #define pf(n) printf("%d",n)
+    #define pff(n,m) printf("%d %d",n,m)
+    #define pffl(n,m) printf("%I64d %I64d",n,m)
+    #define pfl(n) printf("%I64d",n)
+    #define pfs(s) printf("%s",s)
+     
+    #define pb push_back
+    #define pp pair<int, int>
+     
+    using namespace std;
+     
+    int main()
+    {
+        //freopen("in.txt", "r", stdin);
+     
+        int i, j, k;
+        int n, m, t;
+        int a, b, s;
+     
+        sf(t);
+        while(t--)
+        {
+            sff(a, b);
+            sff(n, s);
+     
+            int q = s / n;
+            s -= (min(a, q) * n);
+     
+            if(s <= b)
+                pfs("YES\n");
+            else
+                pfs("NO\n");
+        }
+     
+     
+        return 0;
+    }
