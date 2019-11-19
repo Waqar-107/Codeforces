d65ead2edbb4ec986a2f9249da5df523be63c3f3
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-11-20 00:03:19 +0600

    Create 1255A. Changing Volume.cpp

---
+++ A-set/1255A. Changing Volume.cpp
@@ -0,0 +1,64 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1010100
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+    int a, b;
+
+    sf(t);
+
+    while(t--)
+    {
+        sff(a, b);
+
+        k = abs(a - b);
+
+        int cnt = 0;
+
+        m = k / 5;
+        k -= (5 * m);
+        cnt += m;
+
+        m = k / 2;
+        k -= (2 * m);
+        cnt += m;
+
+        cnt += k;
+
+        pf(cnt); nl;
+    }
+
+
+
+    return 0;
+}
+
