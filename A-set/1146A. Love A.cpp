c04b8e749da2fcc039ee5489db702c5f9f2545c5
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-12-09 19:33:40 +0600

    Create 1146A. Love A.cpp

---
+++ A-set/1146A. Love A.cpp
@@ -0,0 +1,58 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 310
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+    string s;
+
+    cin >> s;
+
+    int ac = 0;
+    n = s.length();
+    for(i = 0; i < n; i++)
+    {
+        if(s[i] == 'a')
+            ac++;
+    }
+
+    while(true)
+    {
+        m = n / 2 + 1;
+
+        if(ac >= m) break;
+        n--;
+    }
+
+    pf(n);
+
+    return 0;
+}
