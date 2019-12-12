d2ee8ea6597feb60f26f6277500ce31a9b946d25
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-12-12 15:35:52 +0600

    Create 1216D. Swords.cpp

---
+++ D-set/1216D. Swords.cpp
@@ -0,0 +1,53 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 210101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m;
+    ll sum = 0;
+
+    sf(n);
+    int a[n];
+    for(i = 0; i < n; i++)
+        sf(a[i]), sum += a[i];
+
+    int mx = *max_element(a, a + n);
+    int z = mx - a[0];
+
+    for(i = 1; i < n; i++)
+        z = __gcd(z, mx - a[i]);
+
+    ll y = (mx * 1ll * n - sum) / z;
+
+    cout << y << " " << z;
+
+    return 0;
+}
