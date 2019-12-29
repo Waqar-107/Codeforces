aa86d623c099b580549a64efbed5e2a66ed7edf8
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2019-12-29 14:03:23 +0600

    Create 1283B. Candies Divison.cpp

---
+++ B-set/1283B. Candies Divison.cpp
@@ -0,0 +1,55 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 210101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, t;
+    int a, b;
+
+    sf(t);
+    while(t--)
+    {
+        sff(n, k);
+
+        a = n / k;
+        b = n % k;
+
+        int ans = 0;
+        ans += (a * k);
+
+        ans += min(b, k / 2);
+
+        pf(ans);
+        nl;
+    }
+
+    return 0;
+}
