0ecaab049f084b4e3251e7d59c91a4cc1c12f8a8
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-04-02 23:29:18 +0600

    Create 1327A. Sum of Odd Integers.cpp

---
+++ A-set/1327A. Sum of Odd Integers.cpp
@@ -0,0 +1,48 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 101010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    ll i, j, k;
+    ll n, m, t;
+
+    sfl(t);
+    while(t--)
+    {
+        sffl(n, k);
+        if(n %2 == k % 2) {
+            if(k * k > n) pfs("No\n");
+            else pfs("YES\n");
+        }
+        else pfs("NO\n");
+    }
+
+    return 0;
+}
