d6218fb91f6438ed0ba9142f02a02530a53722e0
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-04-07 12:37:44 +0600

    Create 1296A. Array with Odd Sum.cpp

---
+++ A-set/1296A. Array with Odd Sum.cpp
@@ -0,0 +1,60 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n");
+#define N 110
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+
+        int odd = 0, even = 0;
+        for(i = 0; i < n; i++)
+        {
+            sf(m);
+            if(m % 2) odd++;
+            else even++;
+        }
+
+        if(even == n) pfs("NO\n");
+        else if(odd == n)
+        {
+            if(n % 2) pfs("YES\n");
+            else pfs("No\n");
+        }
+
+        else pfs("YES\n");
+    }
+
+    return 0;
+}
