09ac9cee03643a96913934b9b0a9748e12b3366c
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-05-31 13:02:29 +0600

    Create 1360B. Honest Coach.cpp

---
+++ B-set/1360B. Honest Coach.cpp
@@ -0,0 +1,54 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 8010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+
+        int a[n];
+        for(i = 0; i < n; i++) sf(a[i]);
+
+        sort(a, a + n);
+
+        int mn = 10000;
+        for(i = 0; i < n - 1; i++)
+            mn = min(mn, a[i + 1] - a[i]);
+
+        pf(mn), nl;
+    }
+
+    return 0;
+}
