777ce3b6456a5121ad04d902c4eca99159de22b5
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-06-14 19:42:20 +0600

    Create 1324A. Yet Another Tetris Problem.cpp

---
+++ A-set/1324A. Yet Another Tetris Problem.cpp
@@ -0,0 +1,59 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 5010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+
+        int a[n];
+        m = 0;
+        for(i = 0; i < n; i++) sf(a[i]), m = max(m, a[i]);
+
+        k = 1;
+        for(i = 0; i < n; i++)
+        {
+            if((m - a[i]) % 2){
+                k = 0;
+                break;
+            }
+        }
+
+        if(k) pfs("YES\n");
+        else pfs("NO\n");
+    }
+
+    return 0;
+}
