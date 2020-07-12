633aa56f1935008c7ba418c482ab936746076f27
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-07-13 01:41:25 +0600

    Create 1374A. Remainder Required.cpp

---
+++ A-set/1374A. Remainder Required.cpp
@@ -0,0 +1,48 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 5010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+    int x, y;
+
+    sf(t);
+    while(t--)
+    {
+        sff(x, y);
+        sf(n);
+
+        m = (n - y) % x;
+        pf(n - m); nl;
+    }
+
+    return 0;
+}
