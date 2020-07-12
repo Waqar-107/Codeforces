c1222791219d53bf55a66103dca07937f68d4782
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-07-13 02:10:23 +0600

    Create 1374C. Move Brackets.cpp

---
+++ C-set/1374C. Move Brackets.cpp
@@ -0,0 +1,57 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 5010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+    string s;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+        int cnt = 0, req = 0;
+        cin >> s;
+
+        for(i = 0; i < n; i++)
+        {
+            if(s[i] == '(') cnt++;
+            else{
+                if(cnt) cnt--;
+                else req++;
+            }
+        }
+
+        pf(req); nl;
+    }
+
+    return 0;
+}
