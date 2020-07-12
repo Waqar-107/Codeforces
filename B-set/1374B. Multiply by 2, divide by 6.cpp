ebce83f6d1ed1e068f7d74f91820b0513ad1b458
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-07-13 01:58:54 +0600

    Create 1374B. Multiply by 2, divide by 6.cpp

---
+++ B-set/1374B. Multiply by 2, divide by 6.cpp
@@ -0,0 +1,60 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 5010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+
+        int cnt2 = 0, cnt3 = 0;
+        while(n % 2 == 0){
+            n /= 2;
+            cnt2++;
+        }
+
+        while(n % 3 == 0){
+            n /= 3;
+            cnt3++;
+        }
+
+        if(cnt2 > cnt3 || n != 1)
+            pf(-1);
+        else
+            pf(2 * cnt3 - cnt2);
+        nl;
+    }
+
+    return 0;
+}
