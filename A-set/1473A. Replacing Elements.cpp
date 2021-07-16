227ecbf666cf3743d90fa3f5f3b792c988f98133
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-07-16 22:41:20 +0600

    Update 1473A. Replacing Elements.cpp

--- A-set/1473A. Replacing Elements.cpp
+++ A-set/1473A. Replacing Elements.cpp
@@ -1,26 +1,66 @@
+/***from dust i have come, dust i will be***/
+
 #include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
 using namespace std;
-typedef long long ll;
-bool flag=false;
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(0);
-cout.tie(0);
-int t;
-cin>>t;
-while(t--){
-    int n,d,temp;
-    cin>>n>>d;
-    int count=0;
-    vector<int>arr(n);
-    for (int i=0;i<n;i++){
-        cin>>arr[i];  
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sff(n, m);
+
+        int a[n];
+        for(i = 0; i < n; i++)
+            sf(a[i]);
+
+        sort(a, a + n);
+
+        for(i = 2; i < n; i++)
+            a[i] = min(a[i], a[0] + a[1]);
+
+        bool flag = true;
+        for(i = 0; i < n; i++)
+        {
+            if(a[i] > m)
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        flag ? pfs("YES\n") : pfs("NO\n");
+    }
+
+    return 0;
 }
-    sort(arr.begin(),arr.end());
-if((arr[0]+arr[1]<=d) || arr[n-1]<=d)
-    cout<<"YES\n";
-else 
-    cout<<"NO\n";
-}
-return 0;
-}
+
+/***https://github.com/Waqar-107/Codeforces***/
