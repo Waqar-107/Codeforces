49c8d8e0080c3c0afc4424131415eef3c0266836
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-08-22 02:21:47 +0600

    Create 1325B. CopyCopyCopyCopyCopy.cpp

---
+++ B-set/1325B. CopyCopyCopyCopyCopy.cpp
@@ -0,0 +1,54 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 10101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        set<int> s;
+        sf(n);
+
+        while(n--)
+        {
+            sf(m);
+            s.insert(m);
+        }
+
+        pf(s.size()), nl;
+    }
+
+    return 0;
+}
+
+/***https://github.com/Waqar-107/Codeforces***/
