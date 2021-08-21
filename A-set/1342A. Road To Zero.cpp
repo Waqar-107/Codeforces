48e55aabf8705ee6aaa5f2ad031981703940e95f
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2021-08-22 02:43:50 +0600

    Create 1342A. Road To Zero.cpp

---
+++ A-set/1342A. Road To Zero.cpp
@@ -0,0 +1,51 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 10101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        ll x, y, a, b;
+        sffl(x, y);
+        sffl(a, b);
+
+        ll diff = abs(x - y);
+        ll ans = diff * a + min(min(x, y) * b, 2 * min(x, y) * a);
+        pfl(ans), nl;
+    }
+
+    return 0;
+}
+
+/***https://github.com/Waqar-107/Codeforces***/
