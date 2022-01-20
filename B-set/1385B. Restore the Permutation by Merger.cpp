9ec0ff7c61897d12b4b11df39adeb920a26b3cff
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2022-01-21 02:36:54 +0600

    Create 1385B. Restore the Permutation by Merger.cpp

---
+++ B-set/1385B. Restore the Permutation by Merger.cpp
@@ -0,0 +1,55 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 10101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        sf(n);
+
+        map<int, int> cnt;
+        for(i = 0; i < n * 2; i++)
+        {
+            sf(m);
+            if(!cnt[m]) pf(m), pfs(" ");
+            cnt[m]++;
+        }
+
+        nl;
+    }
+
+    return 0;
+}
+
+/***https://github.com/Waqar-107/Codeforces***/
