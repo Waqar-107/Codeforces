bd24e034d50f991dc878d2bd5f46d4919fd12b6e
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2022-04-30 03:41:03 +0600

    Create 1669A. Division?.cpp

---
+++ A-set/1669A. Division?.cpp
@@ -0,0 +1,52 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 10101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+
+        m = 0;
+        if(n <= 1399)  m = 4;
+        else if(n >= 1400 && n <= 1599) m = 3;
+        else if(n >= 1600 && n <= 1899) m = 2;
+        else m = 1;
+
+        cout << "Division " << m << endl;
+    }
+
+    return 0;
+}
