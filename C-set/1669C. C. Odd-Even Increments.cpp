91e27a68d45e936e74e24ca2ffbe332f5f8d3cd9
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2022-04-30 03:58:16 +0600

    Create 1669C. C. Odd-Even Increments.cpp

---
+++ C-set/1669C. C. Odd-Even Increments.cpp
@@ -0,0 +1,73 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 10101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> n;
+
+        int oddsInOddIndex = 0, evensInOddIndex = 0;
+        int oddsInEvenIndex = 0, evensInEvenIndex = 0;
+
+        for(i = 1; i <= n; i++)
+        {
+            cin >> m;
+
+            if(i % 2)
+            {
+                if(m % 2)
+                    oddsInOddIndex++;
+                else
+                    evensInOddIndex++;
+            }
+
+            else
+            {
+                if(m % 2)
+                    oddsInEvenIndex++;
+                else
+                    evensInEvenIndex++;
+            }
+        }
+
+        if((evensInOddIndex == 0 || oddsInOddIndex == 0) && (evensInEvenIndex == 0 || oddsInEvenIndex == 0))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
