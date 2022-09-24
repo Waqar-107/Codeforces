4a7b76b7d7cbe1fad25733ca1aa808791cf1564e
Charlie <51523860+Supporter09@users.noreply.github.com>
2022-09-24 14:29:25 +0700

    Fix Indentation P2

--- A-set/667A. Pouring Rain.py
+++ A-set/667A. Pouring Rain.py
@@ -1,14 +1,12 @@
-# from dust i have come, dust i will be
-
 import math
 
 d, h, v, e = map(int, input().split())
 
-drink_s = (4 * v)/(math.pi * (d**2))
+drink_s = (4 * v) / (math.pi * (d**2))
 
 if drink_s > e:
     print("YES")
     result = (h / (drink_s - e))
-    print(round(result,12))
+    print(round(result, 12))
 else:
     print("NO")
