d6c2f8dedeafd7c42e4a05cd0968dcb716ee7f74
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2023-12-30 22:19:18 -0700

    Create 1915A. Odd One Out.cpp

---
+++ A-set/1915A. Odd One Out.cpp
@@ -0,0 +1,52 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 10101
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+
+    while(t--) {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+
+        if(a == b) pf(c);
+        else if(a == c) pf(b);
+        else pf(a);
+
+        nl;
+    }
+
+    return 0;
+}
+
+/***https://github.com/Waqar-107/Codeforces***/
